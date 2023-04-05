1fb2272e2ac79808b3469b267493af8e51791964
17ramdani <17rome01@gmail.com>
2023-04-06 01:49:36 +0700

    Revert "Merge pull request #1 from 17ramdani/rifky_user"
    This reverts commit 76b8219513642022f456d43c4ca0e2a25ce63e94, reversing
    changes made to eea2e9f25a309f312a29a44be9f1cf77cf523ee2.

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,18 +1,11 @@
 @extends('layout.header')
 
 @section('content')
-<div id="wrapper">
-    @include('layout.sidebar')
-    <div id="content-wrapper" class="d-flex flex-column">
-        <div id="content">
-            @include('layout.navbar')
-            <div class="container-fluid" id="container-wrapper">
-                <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                    <h1 class="h3 mb-0 text-gray-800">Hallo, User!</h1>
-                </div>
-            </div>
-        </div>
-        @include('layout.footer')
+    <div id="wrapper">
+        @include('layout.sidebar')
+        @include('layout.navbar')
+
+
     </div>
-</div>
+    @include('layout.footer')
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dailyReport', function () {
-    return view('dailyReport');
-});
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
