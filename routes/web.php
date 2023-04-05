cef048166b93fcb2982ea47f25de2c1f89e0fd63
RIFKY MAULANA <rifkymaulana0612@gmail.com>
2023-04-05 11:20:54 +0700

    fix sidebar and adding dailyReport page

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dailyReport', function () {
+    return view('dailyReport');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
